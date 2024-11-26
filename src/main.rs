14a9f62f1a265ed213cbdaa4e432df68385f2447
艾帅康 <2506377990@qq.com>
2024-11-26 15:32:00 +0800

    feature: testcode for anyhow/thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use ecosysyem::MyError;
+use std::fs;
+
+fn main() -> anyhow::Result<()> {
+    println!("size of MyError is {}", std::mem::size_of::<MyError>()); // 24
+
+    let filename = "foo.txt";
+    let _ = fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+
+    fail_with_error()?;
+
+    Ok(())
+}
+
+fn fail_with_error() -> anyhow::Result<()> {
+    Err(MyError::Custom("This is a custom error".to_string()).into())
 }
